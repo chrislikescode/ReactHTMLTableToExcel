382599b7aff50b9db8b465518b11327a6adb0d00
Christopher Waddington <88463776+chrislikescode@users.noreply.github.com>
2023-03-14 12:05:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ var ReactHTMLTableToExcel = function (_Component) {
       var sheet = String(this.props.sheet);
       var filetype = String(this.props.filetype ? this.props.filetype === 'xlsx' ? 'xlsx' : 'xls' : 'xls');
       var filename = '' + (String(this.props.filename) + '.' + filetype);
-
+      console.log(table);
+      
       var uri = 'data:application/vnd.ms-excel;base64,';
       var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-mic' + 'rosoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta cha' + 'rset="UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:Exce' + 'lWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' + '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></' + 'xml><![endif]--></head><body>{table}</body></html>';
 
